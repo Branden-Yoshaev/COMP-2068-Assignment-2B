1fe55616635f7524215e5869c538170706c02fed
Branden-Yoshaev <brandensam8@icloud.com>
2022-04-18 13:09:48 -0400

    Added login and create account with functionality of being able to create the href to go from one screen to another

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,37 @@ document.addEventListener('DOMContentLoaded', (e)=>{
   }, 2000);
 })
 
+function setForMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList,remove("form__message--success", "form__message-error");
+  messageElement.classList.remove(`form__message--${type}`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginFrom = document.querySelector('#login');
+  const createAccountForm = document.querySelector('#createAccount');
+
+
+  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    e.preventDefault();
+    loginFrom.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
+  });
+
+  document.querySelector("#linkLogin").addEventListener("click", () => {
+    loginFrom.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
+  });
+
+  loginFrom.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // AJAX login
+    setForMessage(loginForm, "Error", "Invaild username and/or password");
+  })
+});
 
 var express = require('express');
 var router = express.Router();
